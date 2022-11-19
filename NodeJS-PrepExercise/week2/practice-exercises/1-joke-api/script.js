828ac3afc4b2d0c42528e6a533faf4fb375ea461
trebektas <trensarbektas@gmail.com>
2022-11-19 17:38:58 +0100

    finished 1-joke-api exercise

--- NodeJS-PrepExercise/week2/practice-exercises/1-joke-api/script.js
+++ NodeJS-PrepExercise/week2/practice-exercises/1-joke-api/script.js
@@ -1,7 +1,7 @@
 /**
  * 1. Chuck Norris programs do not accept input
  * 
- * `GET` a random joke inside the function, using the API: http://www.icndb.com/api/
+ * `GET` a random joke inside the function, using the API: http://www.icndb.com/api/ & https://api.chucknorris.io/jokes/random
  * (use `node-fetch`) and print it to the console. 
  * Make use of `async/await` and `try/catch`
  * 
@@ -10,9 +10,22 @@
  * - Print the entire response to the console to see how it is structured.
  */
 
-function printChuckNorrisJoke() {
-  // YOUR CODE GOES IN HERE
+ import express from 'express';
+ import fetch from 'node-fetch';
 
+ const app = express();
+
+function printChuckNorrisJoke() {
+try {
+  app.get('/', async function (req, res){
+    const response = await fetch('https://api.chucknorris.io/jokes/random');
+    const data = await response.json();
+    res.send(data.value);
+  })
+  app.listen(3000);
+} catch (error) {
+  console.log(error);
+}
 }
 
 printChuckNorrisJoke();
